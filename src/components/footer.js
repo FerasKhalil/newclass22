747fcbc2fb2baccc8542c5db791778e655692578
Firas Hasan <x.firashasan@gmail.com>
2021-05-18 17:53:13 +0300

    lab1

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react'
+
+ class Footer extends PureComponent
+{
+    render()
+    {
+        return (
+            <div>
+                <p>Author : Firas Hasan</p>
+            </div>
+        )
+    }
+}
+
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react'
+
+ class Header extends PureComponent
+{
+    render()
+    {
+        return (
+            <div>
+                <h1>CLASS 1</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Header;
